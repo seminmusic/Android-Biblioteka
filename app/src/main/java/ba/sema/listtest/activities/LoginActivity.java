03c01f3218b7bcba1e9b638d7751ee15efc0d06b
semin@sema-pc <music.semin@gmail.com>
2017-12-28 22:08:32 +0100

    Add Login Activity

---
+++ app/src/main/java/ba/sema/listtest/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package ba.sema.listtest.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ba.sema.listtest.R;
+
+
+public class LoginActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
