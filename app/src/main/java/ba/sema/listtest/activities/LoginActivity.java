f38649fec0aacf8a43623e8a873d4baf4c48a298
semin@bamcard-windows <music.semin@gmail.com>
2017-12-29 15:52:11 +0100

    Login Button

--- app/src/main/java/ba/sema/listtest/activities/LoginActivity.java
+++ app/src/main/java/ba/sema/listtest/activities/LoginActivity.java
@@ -1,17 +1,42 @@
 package ba.sema.listtest.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import ba.sema.listtest.R;
 
 
 public class LoginActivity extends AppCompatActivity
 {
+    private String TAG = LoginActivity.class.getSimpleName();
+    private EditText inputUsername;
+    private EditText inputPassword;
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputUsername = (EditText) findViewById(R.id.login_username);
+        inputPassword = (EditText) findViewById(R.id.login_password);
+        btnLogin = (Button) findViewById(R.id.login_button);
+
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        });
     }
 }
